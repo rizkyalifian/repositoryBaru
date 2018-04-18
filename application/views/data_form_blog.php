9c4a6fee2c7266bcebcf45faed6272ab334e83f3
rizkyalifian <37797926+rizkyalifian@users.noreply.github.com>
2018-04-19 00:01:17 +0700

    Add files via upload

--- application/views/data_form_blog.php
+++ application/views/data_form_blog.php
@@ -26,14 +26,18 @@
     <br><br><br>
 
   <div class="container">
-    <h1><?=$tipe?> Articel</h1>
+  <?php echo validation_errors(); ?>
+    <?php 
+      echo form_open('v_blog/add', array ('enctype'=>'multipart/form-data'));
+      ?>
+    <h1> Articel</h1>
     <form method="post" class="form-horizontal" enctype="multipart/form-data">
       <div class="form-group">
         <label class="control-label col-sm-2">
           Judul
         </label>
         <div class="col-sm-10">
-          <input type="text" class="form-control" name="judul" value="<?=isset($default['judul'])? $default['judul'] : ""?>" required>
+          <input type="text" class="form-control" name="judul" value="<?=isset($default['judul'])? $default['judul'] : ""?>" >
         </div>
       </div>
       <div class="form-group">
@@ -41,7 +45,7 @@
           Date          
         </label>
         <div class="col-sm-10">
-          <input type="date" required class="form-control" name="date" value="<?=isset($default['date'])? $default['date'] : ""?>">
+          <input type="date"  class="form-control" name="date" value="<?=isset($default['date'])? $default['date'] : ""?>">
         </div>
       </div>
       <div class="form-group">
@@ -49,16 +53,56 @@
           Content         
         </label>
         <div class="col-sm-10">
-          <textarea name="content" class="form-control" required><?=isset($default['content'])? $default['content'] : ""?></textarea>
+          <textarea name="content" class="form-control" ><?=isset($default['content'])? $default['content'] : ""?></textarea>
         </div>
       </div>
       <div class="form-group">
           <label class="control-label col-sm-2">Gambar :</label>
          
           <div class="col-sm-10">
-            <span class="input-group-addon"><input type="file" required name="gambar" class="file"></span>
+            <span class="input-group-addon"><input type="file"  name="gambar" class="file"></span>
           </div><br>
         </div>
+         <div class="form-group">
+        <label class="control-label col-sm-2">
+          Link_Download
+        </label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" name="link_download" value="<?=isset($default['link_download'])? $default['link_download'] : ""?>" >
+        </div>
+      </div>
+       <div class="form-group">
+        <label class="control-label col-sm-2">
+          Pengembang
+        </label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" name="Pengembang" value="<?=isset($default['Pengembang'])? $default['Pengembang'] : ""?>" >
+        </div>
+      </div>
+       <div class="form-group">
+        <label class="control-label col-sm-2">
+          Publisher
+        </label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" name="Publisher" value="<?=isset($default['Publisher'])? $default['Publisher'] : ""?>" >
+        </div>
+      </div>
+       <div class="form-group">
+        <label class="control-label col-sm-2">
+          Platforms
+        </label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" name="Platforms" value="<?=isset($default['Platforms'])? $default['Platforms'] : ""?>" >
+        </div>
+      </div>
+       <div class="form-group">
+        <label class="control-label col-sm-2">
+          Rating
+        </label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" name="Rating" value="<?=isset($default['Rating'])? $default['Rating'] : ""?>" >
+        </div>
+      </div>
       <center>
       <input class="btn btn-primary" type="submit" name="simpan" value="simpan">
       </center>
